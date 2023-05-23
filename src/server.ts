d4b40c4de1be3ef8ec2386286bb289025d618974
Luan <luanchristian.rochadocarmo@gmail.com>
2023-05-23 11:10:17 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello world!'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+
+  .then(() => {
+    console.log('listening on port')
+  })
