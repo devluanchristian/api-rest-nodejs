bc5e1e83327bd1524643a3c0be834ef881794429
Luan <luanchristian.rochadocarmo@gmail.com>
2023-05-23 15:14:40 -0300

    :sparkles: feat: funções para realização de queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  // inserindo dados na tabela transactions
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: randomUUID(),
+  //     title: 'Testando api 1',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+  // return transaction
+  // ---------------------------------------------
+  // buscando os dados que foram inseridos
+  // const transactions = await knex('transactions').select('*')
+  // return transactions
+  // ---------------------------------------------
+  // buscando somente um dado que foi inserido
+  // const transaction = await knex('transactions')
+  //   .where('amount', 1000)
+  //   .select('*')
+  // return transaction
 })
 
 app
