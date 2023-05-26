a4162b36259d8cd2c9a3e50de4ce75271f7a0f8b
Luan <luanchristian.rochadocarmo@gmail.com>
2023-05-26 11:04:12 -0300

    refactor: add middlewares global

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,32 +2,42 @@ import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
+import { checkSessionIdExist } from '../middlewares/check-session-id-exists'
 
 // Cookies <--> Formas dagente manter contexto entre requisições
 
 // plugin
 export async function transactionsRoutes(app: FastifyInstance) {
   // busca transações
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.get('/', { preHandler: checkSessionIdExist }, async (request) => {
+    const { sessionId } = request.cookies
+    const transactions = await knex('transactions')
+      .select('*')
+      .where('session_id', sessionId)
     return { transactions }
   })
   // busca somente uma transação
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: checkSessionIdExist }, async (request) => {
     const getTransactionsParamsSchema = z.object({
       id: z.string().uuid(),
     })
     const { id } = getTransactionsParamsSchema.parse(request.params)
+    const { sessionId } = request.cookies
     const transactions = await knex('transactions')
       .select()
-      .where('id', id)
+      .where({
+        session_id: sessionId,
+        id,
+      })
       .first()
     return { transactions }
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: checkSessionIdExist }, async (request) => {
+    const { sessionId } = request.cookies
     const summary = await knex('transactions')
       .sum('amount', { as: 'amount' })
+      .where('session_id', sessionId)
       .first()
     return { summary }
   })
